f2cd532c25174567b0e08ded01d207cac52c64e4
Willem Jan Glerum <wjglerum@gmail.com>
2020-11-29 13:48:18 +0100

    Custom auth filter with logging

--- src/main/java/nl/wjglerum/ExampleResource.java
+++ src/main/java/nl/wjglerum/ExampleResource.java
@@ -1,16 +1,29 @@
 package nl.wjglerum;
 
+import org.jboss.logging.Logger;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
 
-@Path("/resteasy/hello")
+@Path("/")
 public class ExampleResource {
+
+    private static final Logger LOGGER = Logger.getLogger(ExampleResource.class);
+
+    @Context
+    SecurityContext context;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        LOGGER.info(context.getUserPrincipal().getName());
+        LOGGER.info(context.getAuthenticationScheme());
+        LOGGER.info(context.isSecure());
+        LOGGER.info(context.isUserInRole("admin"));
         return "hello";
     }
 }
